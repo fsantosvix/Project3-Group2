96e3e0a1ca3d30afc52563f418c6eddf4bf45027
Oliver Uy <uyoliverjames@gmail.com>
2024-04-15 21:08:00 +0930

    merged codes

---
+++ Codes/config.py
@@ -0,0 +1,13 @@
+# Uncomment and replace with your details
+# DB_USERNAME = 'your_username'
+# DB_PASSWORD = 'your_password'
+# DB_HOST = 'your_host'
+# DB_PORT = 'your_port'
+# DB_NAME = 'your_database_name'
+
+# sample details - comment this and follow the above
+DB_USERNAME = 'postgres'
+DB_PASSWORD = 'postgres'
+DB_HOST = 'localhost'
+DB_PORT = '5432'
+DB_NAME = 'Street_lights'
